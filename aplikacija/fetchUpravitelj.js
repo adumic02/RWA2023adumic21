bf723c33c4f36037d096db33e1f8b7f6441473c3
adumic21 <dumic.antonio@gmail.com>
2023-12-15 15:34:18 +0100

    Ispravka

--- aplikacija/fetchUpravitelj.js
+++ aplikacija/fetchUpravitelj.js
@@ -1,5 +1,4 @@
 const GlumciPretrazivanje = require("./glumciPretrazivanje.js");
-const Korisnici = require("./korisniciPrikaz.js");
 
 class FetchUpravitelj {
 	constructor(api_kljuc, appStranicenje) {

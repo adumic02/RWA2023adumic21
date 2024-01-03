3b83456c42bba0ccb4bc06a9043500a1fdbeb96e
adumic21 <dumic.antonio@gmail.com>
2024-01-03 23:29:16 +0100

    TOTP, popunjavanje baze

--- aplikacija/js/detalji.js
+++ aplikacija/js/detalji.js
@@ -32,20 +32,20 @@ function prikaziDetaljeGlumca(glumac) {
 	tablica +=
 		"<tr><th>Ime i prezime</th><th>Slika</th><th>Biografija</th><th>Popis alternativna imena</th><th>Vrsta</th><th>Popularnost</th><th>Datum rođenja</th><th>Mjesto rođenja</th><th>Datum smrti</th><th>Vanjska stranica</th><th>Popis poznatih naslova</th></tr>";
 	tablica += "<tr>";
-	tablica += "<td>" + glumac.ime_prezime + "</td>";
+	tablica += `<td>${glumac.ime_prezime}</td>`;
 	tablica +=
 		"<td><img src='https://image.tmdb.org/t/p/original/" +
 		glumac.slika +
 		"' width='100' alt='slika_'/></td>";
-	tablica += "<td>" + glumac.biografija + "</td>";
-	tablica += "<td>" + glumac.alternativna_imena + "</td>";
-	tablica += "<td>" + glumac.vrsta + "</td>";
-	tablica += "<td>" + glumac.popularnost + "</td>";
-	tablica += "<td>" + glumac.datum_rodenja + "</td>";
-	tablica += "<td>" + glumac.mjesto_rodenja + "</td>";
-	tablica += "<td>" + glumac.datum_smrti + "</td>";
-	tablica += "<td>" + glumac.vanjska_stranica + "</td>";
-	tablica += "<td>" + glumac.naslovi + "</td>";
+	tablica += `<td>${glumac.biografija}</td>`;
+	tablica += `<td>${glumac.alternativna_imena}</td>`;
+	tablica += `<td>${glumac.vrsta}</td>`;
+	tablica += `<td>${glumac.popularnost}</td>`;
+	tablica += `<td>${glumac.datum_rodenja}</td>`;
+	tablica += `<td>${glumac.mjesto_rodenja}</td>`;
+	tablica += `<td>${glumac.datum_smrti}</td>`;
+	tablica += `<td><a href="${glumac.vanjska_stranica}" target="_blank">${glumac.vanjska_stranica}</a></td>`;
+	tablica += `<td>${glumac.naslovi}</td>`;
 	tablica += "</tr>";
 
 	tablica += "</table>";
